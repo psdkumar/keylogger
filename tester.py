4a95d94fd062cc171d64766e43c54ae150f87075
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2016-11-02 18:33:59 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,16 +1,22 @@
 import os
 import keylogger as kl
-from data_framer import DataFramer
+from data_framer import TestingDataFramer
 
 def start_test() :
     #running keylogger
     print "type for 10 seconds......"
     kl.start_keylogger(10)
 
-    #dheeraj start from here . create test and train csv files
+def create_final_CSVs() :
+	tdf = TestingDataFramer()
+	tdf.get_all_paths(os.path.join(os.getcwd(), "Testing"))
+	tdf.extract_data()
+	tdf.make_dataframe()
+	tdf.make_csv()
 
 if __name__ == '__main__':
     start_test()
+    create_final_CSVs()
 
 
 
